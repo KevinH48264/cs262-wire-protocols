3792799564b2c36849f0f8fe0be58952799bfa55
KevinH48264 <1kevin.huang@gmail.com>
2023-02-14 22:04:55 -0500

    functions included

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import _thread
 import sys
 import os
 import subprocess
@@ -47,11 +48,13 @@ def bind_socket():
 
 # Establish connection with a client (socket must be listening)
 def socket_accept():
-    conn, address = s.accept()
-    print("Connection has been established! |" + " IP " + address[0] + " | Port" + str(address[1]))
-    receive_commands(conn)
-    conn.close()
+    while True:
+        conn, address = s.accept()
+        print("Connection has been established! |" + " IP " + address[0] + " | Port" + str(address[1]))
 
+        # allow for multiple connections to be made
+        _thread.start_new_thread(receive_commands, (conn, address))
+    s.close()
 
 # Send commands to client/victim or a friend
 def receive_commands(conn):
@@ -67,19 +70,58 @@ def receive_commands(conn):
             res = "create_account"
 
         # b - allow clients to log in
+        if 'log_in ' in input_cmd:
+
+            # k - server delivers undelivered messages to a particular user if they logged in
+            check_queue(input_cmd[8:], conn)
 
         # b - allow client to list accounts by text wildcard
 
         # k - allow client to send a message to a recipient, and queue if the recipient isn't logged in
+        # to send a message: "send_message_to [INSERT RECIPIENT] message: [INSERT MESSAGE]"
+        if 'send_message_to ' in input_cmd:
+            res = send_message(input_cmd)
 
-        # k - server delivers undelivered messages to a particular user if they logged in
 
         # b- allow client to delete an account
 
         # k - allow clients to quit connection
+        if 'quit' in input_cmd[:4]:
+            # mark that the sender has closed their connection
+            sender = accounts.keys()[accounts.values().index(conn)]
+            accounts[sender] = None
 
         conn.send(str.encode(res))
 
+def check_queue(user, conn):
+    # send messages to user
+    for msg in queues[user]:
+        conn.send(str.encode(msg))
+
+def send_message(input_cmd, conn):
+    # ensure the formatting for sending a message is correct
+    if 'message: ' not in input_cmd:
+        res = "please send a message using this format: 'send_message_to [INSERT RECIPIENT] message: [INSERT MESSAGE]'"
+    
+    recipient = input_cmd[17:input_cmd.find("message: ")]
+    message = input_cmd[input_cmd.find("message: ") + 9:]
+
+    if recipient in accounts.keys(): 
+        # recipient exists
+        recipient_conn = accounts[recipient]
+        
+        if recipient_conn:
+            # recipient is online and message can be delivered
+            recipient_conn.send(str.encode(message))
+        else:
+            # recipient is offline and message should be stored in queue
+            sender = accounts.keys()[accounts.values().index(conn)]
+            queues[recipient] = queues[recipient].append([sender + " sent you a message: " + message])
+    else:
+        # recipient does not exist
+        res = "error: the recipient " + recipient + " does not exist, please have them create an account before you can send a message to them"
+    
+    return res
 
 def main():
     create_socket()
